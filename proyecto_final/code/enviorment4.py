1c355ae2a220944c391fcde1b75126eb9b703e31
Ari Rafael Cohen <aricohen98@gmail.com>
2023-12-07 10:49:00 -0300

    working agent, with new scenario

--- proyecto_final/code/enviorment4.py
+++ proyecto_final/code/enviorment4.py
@@ -13,29 +13,18 @@ class VizDoom(gym.Env):
 
     # metadata = {"render_modes": ["human"], "render_fps": 30}
 
-    def __init__(self, render = False, env_selection = "basic"):
+    def __init__(self, render = False):
         super().__init__()
         ''' '''
         ''' '''
         self.game = vzd.DoomGame()
-        if env_selection == "basic":
-            self.game_path = "./basic.cfg"
-            # Causes episodes to finish after 200 tics (actions)
-            #instead of 300 (default)
-            self.game.set_episode_timeout(200)
-        if env_selection == "defend_the_line":
-            self.game_path = "C:/Users/arico/Documents/UNC/3ro/Inteligencias Artificial/ia-uncuyo-2023-develop/proyecto_final/code/ViZDoom-master/scenarios/new_defend_the_line.cfg"
-            # self.game_path = "./defend_the_line.cfg"
 
-        if env_selection == "defend_the_center":
-            self.game_path = "./defend_the_center.cfg"
-
-        self.game.load_config(self.game_path)
+        self.game.load_config("./basic.cfg")
 
         # Adds buttons that will be allowed to use.
-        # self.game.set_available_buttons(
-        #     [vzd.Button.MOVE_LEFT, vzd.Button.MOVE_RIGHT, vzd.Button.ATTACK]
-        # )
+        self.game.set_available_buttons(
+            [vzd.Button.MOVE_LEFT, vzd.Button.MOVE_RIGHT, vzd.Button.ATTACK]
+        )
 
         # Buttons that will be used can be also checked by:
         print("Available buttons:", [b.name for b in self.game.get_available_buttons()])
@@ -50,12 +39,15 @@ class VizDoom(gym.Env):
         self.game.set_automap_buffer_enabled(True)
 
         # Adds game variables that will be included in state.  #Optional because is config by default
-        # self.game.set_available_game_variables([vzd.GameVariable.AMMO2])#,vzd.GameVariable.HEALTH])
-
+        self.game.set_available_game_variables([vzd.GameVariable.AMMO2])
         print(
             "Available game variables:",
             [v.name for v in self.game.get_available_game_variables()],
         )
+
+        # Causes episodes to finish after 200 tics (actions)
+        self.game.set_episode_timeout(200) #instead of 300 (default)
+
         # Makes episodes start after 10 tics (~after raising the weapon)
         # self.game.set_episode_start_time(10)
 
@@ -66,7 +58,7 @@ class VizDoom(gym.Env):
             self.game.set_window_visible(True)
 
         # Sets the living reward (for each move) to -1
-        # self.game.set_living_reward(-1.2)
+        self.game.set_living_reward(-1.2)
         
         # Sets the shooting reward (for each shoot) to -10
         # self.game.set_shotgun_reward(-10)
@@ -74,22 +66,21 @@ class VizDoom(gym.Env):
         # Enables labeling of in-game objects labeling (turned off by default).
         # self.game.set_labels_buffer_enabled(True)
         # Enables information about all objects present in the current episode/level.
+        self.game.set_objects_info_enabled(True)
 
         # Enables information about all sectors (map layout).
         # self.game.set_sectors_info_enabled(True)
-        # if env_selection == "basic":
-        #     self.game.set_objects_info_enabled(True)
-        #     # Enables information about all vertices (map layout).
-        #     self.game.add_available_game_variable(vzd.GameVariable.POSITION_X)
-        #     self.game.add_available_game_variable(vzd.GameVariable.POSITION_Y)
+
+        self.game.add_available_game_variable(vzd.GameVariable.POSITION_X)
+        self.game.add_available_game_variable(vzd.GameVariable.POSITION_Y)
 
         # In order to visualize depth buffer, you need to enable it first
         # self.game.set_depth_buffer_enabled(True)
 
         # Initialize the game. Further configuration won't take any effect from now on.
         self.game.init()
-        self.ammo = 50
-        self.reward = 0
+        
+
         # Define action and observation space
         # They must be gym.spaces objects
         # Example when using discrete actions:
@@ -127,38 +118,22 @@ class VizDoom(gym.Env):
         return cropScreenBuffer
 
     def step(self, action):
-        # self.game.send_game_command("give NewAmmo")
-        # self.game.send_game_command("give NewAmmo")
         #actions = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
         #actions[0]= Move Left | actions[1]= Move Right | actions[2]= Attack 
-        state = self.game.get_state()
         actions = np.identity(3, dtype=np.uint8) # [[1,0,0],[0,1,0],[0,0,1]]
-        # if action == 0 or action == 1:
-        #     self.reward = self.reward - 0.02
-        # if self.game_path == "./defend_the_line.cfg":
-        #     if self.ammo == 0:
-        #         self.action_space = spaces.Discrete(2)
-        #         self.reward = self.game.get_last_reward()
-        #         self.ammo -= 1
-        #     if action == 2 and self.ammo <= 0:
-        #         state = self.game.get_state() #get the state of the game with the screen buffer and game variables
-        #         grayScaledImg = self.grayScale()
-        #         info = {
-        #             "AMMO2": 0,
-        #             "HEALTH": state.game_variables[1],
-        #             }
-        #         self.reward -=1
-        #         # reward = self.game.get_last_reward()-1
-        #         # print("reward: ",self.reward)
-        #         return grayScaledImg, self.reward , self.game.is_episode_finished(), False,info
         # action = random.choice(actions) #randomly choose an action from the set of posible actions
-        frameSkip = 2 #number of frames to skip
+        frameSkip = 1 #number of frames to skip
         reward = self.game.make_action(actions[action],frameSkip) #make an action and get the reward
         # reward = self.game.make_action(action)
         if self.game.get_state():
             state = self.game.get_state() #get the state of the game with the screen buffer and game variables
             img = state.screen_buffer #get the screen buffer, the image that is rendered of the game frame by frame in an array of pixels in RGB24 format
             grayScaledImg = self.grayScale()
+            # grayscaledImg = state.depth_buffer #get the depth buffer, the image that is rendered of the game frame by frame in an array of pixels in 8-bit gray channel format
+            # grayscaledImg = np.repeat(state.depth_buffer[...], repeats=2, axis=1)
+            # shape=(48,64,3)
+            # grayscaledImg = cv2.resize(grayscaledImg, shape[::-1])
+            # grayscaledImg = np.stack([grayscaledImg] * 3, axis=2)
             """  get enemy and player position """
             # enemy = state.objects[0] if state.objects[0].name == "Cacodemon" else state.objects[1]
             # player = state.objects[0] if state.objects[0].name == "DoomPlayer" else state.objects[1]
@@ -168,26 +143,16 @@ class VizDoom(gym.Env):
             # print("X: ",player.position_x,", Y: ",player.position_y)
             # time.sleep(4)
             """  """
-            info = {}
-        
-                # if np.array_equal(actions[action], [False,False,True]):
-                #     self.ammo -= 1
-            info = {
-                "AMMO2": state.game_variables[0],
-                "HEALTH": state.game_variables[1],
-                }
-            # print(info,end=" ")
-                # info["ammo"] = state.game_variables[0]
+            # print()
+            info = {"ammo": 0}
+            info["ammo"] = state.game_variables[0]
         else:
             grayScaledImg = np.zeros(self.observation_space.shape)
-            info = {
-                        "AMMO2": 0,
-                        "HEALTH": 0,
-                        }
+            info = {"ammo": 0}
             # info["ammo"] = state.game_variables[0]
         truncated = False  # Truncation to be handled by the TimeLimit wrapper
-        # print("/treward:", reward, end=" ")
-        return grayScaledImg, reward+self.reward, self.game.is_episode_finished(), truncated,info
+        # print("\treward:", reward, end=" ")
+        return grayScaledImg, reward, self.game.is_episode_finished(), truncated,info
 
     def reset(
         self,
@@ -196,9 +161,41 @@ class VizDoom(gym.Env):
         options: Optional[dict] = None,
     ):
         super().reset(seed=seed)
+        # if seed is not None:
+        #     self.game.set_seed(seed)
         self.game.new_episode()
+        # self.game.new_episode()
+        state = self.game.get_state()
+        enemy = state.objects[0] if state.objects[0].name == "Cacodemon" else state.objects[1]
+        player = state.objects[0] if state.objects[0].name == "DoomPlayer" else state.objects[1]
+        positions = (abs(enemy.position_y) - abs(player.position_y))
+        # print("resta: ", positions)
+        # print(enemy.name)
+        # print("X: ",enemy.position_x,", Y: ",enemy.position_y)
+        # print(player.name)
+        # print("X: ",player.position_x,", Y: ",player.position_y)
+        while positions > (-35) and positions < 50: #if the enemy is too close to the player
+            # print("resta: ", positions)
+            # print(enemy.name)
+            # print("X: ",enemy.position_x,", Y: ",enemy.position_y)
+            if enemy.position_y > 0 and random.random() < 0.8:
+                self.game.new_episode()
+            # print(player.name)
+            # print("X: ",player.position_x,", Y: ",player.position_y)
+            # time.sleep(10)
+            self.game.new_episode()
+            state = self.game.get_state()
+            enemy = state.objects[0] if state.objects[0].name == "Cacodemon" else state.objects[1]
+            player = state.objects[0] if state.objects[0].name == "DoomPlayer" else state.objects[1]
+            positions = (abs(enemy.position_y) - abs(player.position_y))
+            # print("resta: ", positions)
+        # cont = 0
+        # if random.random() < 0.5:
+        #     while (enemy.position_y > 50) and cont < 3:
+        #         # if random.random() < 0.5:
+        #         self.game.new_episode()
+        #         cont+=1
         grayScaledImg = self.grayScale()
-        self.reward = 0
         return  grayScaledImg, {}
 
     def render(self,render):
@@ -206,11 +203,50 @@ class VizDoom(gym.Env):
             self.game.set_window_visible(True)
             
         return self.reset()
-
+    
+    def reset2(
+        self,
+        *,
+        seed: Optional[int] = None,
+        options: Optional[dict] = None,
+        cont
+    ):
+        super().reset(seed=seed)
+        # if seed is not None:
+        #     self.game.set_seed(seed)
+        self.game.new_episode(f"./proyecto_final/replays/episode{cont}_rec.lmp")
+        # self.game.new_episode()
+        state = self.game.get_state()
+        enemy = state.objects[0] if state.objects[0].name == "Cacodemon" else state.objects[1]
+        player = state.objects[0] if state.objects[0].name == "DoomPlayer" else state.objects[1]
+        positions = (abs(enemy.position_y) - abs(player.position_y))
+        # print("resta: ", positions)
+        # print(enemy.name)
+        # print("X: ",enemy.position_x,", Y: ",enemy.position_y)
+        # print(player.name)
+        # print("X: ",player.position_x,", Y: ",player.position_y)
+        while positions > (-30) and positions < 30: #if the enemy is too close to the player
+            # print("resta: ", positions)
+            # print(enemy.name)
+            # print("X: ",enemy.position_x,", Y: ",enemy.position_y)
+            # print(player.name)
+            # print("X: ",player.position_x,", Y: ",player.position_y)
+            self.game.new_episode(f"./proyecto_final/replays/episode{cont}_rec.lmp")
+            state = self.game.get_state()
+            enemy = state.objects[0] if state.objects[0].name == "Cacodemon" else state.objects[1]
+            player = state.objects[0] if state.objects[0].name == "DoomPlayer" else state.objects[1]
+            positions = (abs(enemy.position_y) - abs(player.position_y))
+        # cont = 0
+        # if random.random() < 0.5:
+        #     while (enemy.position_y > 50) and cont < 3:
+        #         # if random.random() < 0.5:
+        #         self.game.new_episode()
+        #         cont+=1
+        grayScaledImg = self.grayScale()
+        return  grayScaledImg, {}
         
     def get_total_reward(self):
-        rw = self.game.get_total_reward() + self.reward
-        return rw
+        return self.game.get_total_reward()
     
     def close(self):
         self.game.close()
---
+++ proyecto_final/code/main2.py
@@ -0,0 +1,180 @@
+import random
+import os
+import numpy as np
+import enviorment4
+import enviorment
+import cv2
+from stable_baselines3.common.env_checker import check_env
+import vizdoom as vzd
+import time
+''' game configuration'''
+# # Create DoomGame instance. It will run the game and communicate with you.
+# game = vzd.DoomGame()
+# # Adds buttons that will be allowed to use.
+# game.load_config("./basic.cfg")
+
+# game.set_available_buttons(
+#     [vzd.Button.MOVE_LEFT, vzd.Button.MOVE_RIGHT, vzd.Button.ATTACK]
+# )
+# # Buttons that will be used can be also checked by:
+# print("Available buttons:", [b.name for b in game.get_available_buttons()])
+# # game.init()
+
+# game.set_screen_resolution(vzd.ScreenResolution.RES_640X480)
+
+# # Sets the screen buffer format. Not used here but now you can change it. Default is CRCGCB.
+# game.set_screen_format(vzd.ScreenFormat.RGB24)
+
+# # Enables buffer with a top-down map of the current episode/level (turned off by default).
+# game.set_automap_buffer_enabled(True)
+
+
+# # Adds game variables that will be included in state.  #Optional because is config by default
+# game.set_available_game_variables([vzd.GameVariable.AMMO2])
+# print(
+#     "Available game variables:",
+#     [v.name for v in game.get_available_game_variables()],
+# )
+
+# # Causes episodes to finish after 200 tics (actions)
+# game.set_episode_timeout(200) #instead of 300 (default)
+
+# # Makes episodes start after 10 tics (~after raising the weapon)
+# game.set_episode_start_time(10)
+
+# # Makes the window appear (turned on by default)
+# game.set_window_visible(True)
+
+# # Sets the living reward (for each move) to -1
+# game.set_living_reward(-1)
+
+# #in order to visualize depth buffer, you need to enable it first
+# game.set_depth_buffer_enabled(True)
+
+# # Initialize the game. Further configuration won't take any effect from now on.
+# game.init()
+
+'''end of game configuration'''
+
+
+# game.new_episode() #Starts a new episode
+
+
+
+'''experimental screen buffers tests'''   
+# state = game.get_state() #get the state of the game with the screen buffer and game variables
+# img = state.screen_buffer #get the screen buffer, the image that is rendered of the game frame by frame in an array of pixels in RGB24 format
+# print(img)
+# print(img.shape) # set by set_screen_resolution(vzd.ScreenResolution.RES_640X480)
+
+# # # Depth buffer, always in 8-bit gray channel format.
+# depth = state.depth_buffer
+# cv2.resize(depth, (160, 120)) # resize in case that resolution is too big to procces quickly
+# print(depth)
+# cv2.imshow("ViZDoom Depth Buffer", depth)
+# print(depth.shape)
+# cv2.waitKey(0) # Press Enter to continue
+# exit()
+''' end of experimental screen buffers tests'''
+
+
+
+
+# It will check your custom environment and output additional warnings if needed
+# exit()
+from stable_baselines3 import PPO, A2C
+from stable_baselines3.common.callbacks import CheckpointCallback
+# grayscaledImg, reward, done, truncated,ammo = env.step(random.choice([0,1,2]))
+# env = enviorment.VizDoom()
+# print(check_env(env))
+
+
+env = enviorment4.VizDoom()
+# Save a checkpoint every 1000 steps
+checkpoint_callback = CheckpointCallback(
+  save_freq=25000,
+  save_path="./proyecto_final/logs/TurningPPO",
+  name_prefix="PPO_model",
+  save_replay_buffer=True,
+  save_vecnormalize=True,
+)
+
+# # modelElTr = A2C("CnnPolicy", env, verbose=1,learning_rate=0.003, batch_size=128, gae_lambda=0)
+# # model2 = A2C("CnnPolicy", env,tensorboard_log="./proyecto_final/tensorLogs" , verbose=1,learning_rate=0.003)
+# model = PPO("CnnPolicy", env,tensorboard_log="./proyecto_final/tensorLogs",verbose=1,learning_rate=0.00027, batch_size=64)
+# model.learn(total_timesteps = 100000, callback=checkpoint_callback)
+
+# remove to demonstrate saving and loading
+# env.close()
+# del env
+# env = enviorment4.VizDoom(True)
+# model.set_env(env)
+# model.learn(total_timesteps = 100000, callback=checkpoint_callback2)
+# env.close()
+# del env
+# env = enviorment.VizDoom(True,env_selection="defend_the_line")
+
+
+# env = enviorment.VizDoom(False,env_selection="defend_the_line")
+# model = PPO.load("./proyecto_final/logs/PPO_model_90000_steps")
+# PPO.set_env(model,env)
+# model.learn(total_timesteps = 150000, callback=checkpoint_callback)
+# env.close()
+# del env
+# del model
+
+
+env = enviorment.VizDoom(True,env_selection="defend_the_line")
+model = PPO.load("./proyecto_final/logs/TurningPPO/PPO_model_150000_steps")
+cont = 0
+# # # env.game.new_episode(f"./proyecto_final/replays/episode{cont}_rec.lmp")
+obs, _ = env.reset()
+while True:
+  action, _states = model.predict(obs)
+  # print(_states)
+  # print(action)
+  obs, reward, done, _ ,info = env.step(action)
+  # print("act: ",action," rew: ",env.get_total_reward())
+  time.sleep(0.028)
+  if done:
+    print("Episode finished, reward:", env.get_total_reward())
+    obs,_ = env.reset()
+    cont +=1
+    if cont == 1:
+      env.game.close()
+      break
+    
+# print("shapee ",grayscaledImg.shape)  
+# print(env.observation_space)
+# cv2.imshow("ViZDoom Depth Buffer", grayscaledImg)
+# cv2.waitKey(0)
+# while not done:
+#     grayscaledImg, reward, done, ammo = env.step(random.choice([0,1,2]))
+#     print(env.action_space)
+
+'''random movement agent test'''
+# episodes = 5
+
+# for episode in range(episodes):
+#     game.new_episode() #Starts a new episode
+#     while not game.is_episode_finished(): #check if episode is not finished
+#         state = game.get_state() #get the state of the game with the screen buffer and game variables
+#         img = state.screen_buffer #get the screen buffer, the image that is rendered of the game frame by frame in an array of pixels in RGB24 format
+#         ammo = state.game_variables
+#         action = random.choice(actions) #randomly choose an action from the set of posible actions
+#         reward = game.make_action(action) #make an action and get the reward
+#         print("\treward:", reward, end=" ")
+#         time.sleep(0.02)
+#     print("Result:", game.get_total_reward()) #print the total reward
+#     time.sleep(2)
+'''end of random movement agent test'''
+
+
+
+
+'''Gym implemetation '''
+
+
+# class CustomEnv(gym.Env):
+   
+
